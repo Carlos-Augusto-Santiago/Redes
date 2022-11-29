cef2226d3de0a7bf29721dff9912f029d0fb409f
Carlos Santiago <nwasantiago611@gmail.com>
2022-11-29 16:19:52 -0600

    Cambios en cliente y servidor, se rompe al recibir el producto a comprar

--- Primer-parcial/Practica/Practica/Cliente/Cliente.java
+++ Primer-parcial/Practica/Practica/Cliente/Cliente.java
@@ -1,4 +1,3 @@
-package Cliente;
 
 import java.net.*;
 import java.io.*;
@@ -13,14 +12,36 @@ public class Cliente {
             System.out.printf("\nEscriba el puerto: ");
             int pto = Integer.parseInt(br.readLine());
             Socket cl = new Socket(host, pto);
+            DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+            DataInputStream dis = new DataInputStream(cl.getInputStream());
 
             // Recibir el archivo serializado
+            // recibir la cantidad de productos
+            int numProducts = dis.readInt();
             // se recibe el nombre del archivo
-            DataInputStream dis = new DataInputStream(cl.getInputStream());
             String nombre = dis.readUTF();
             System.out.println("Recibimos el archivo:" + nombre);
             // se recibe el tamaño del archivo
             long tam = dis.readLong();
+
+            // Recibir el archivo serializado
+            recieveFile(nombre, tam, dis);
+
+            // Deserializando el archivo
+            deserialize(nombre, numProducts);
+
+            // Enviando el producto que se esta comprando
+            buying(br, dos);
+
+            dis.close();
+            cl.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void recieveFile(String nombre, long tam, DataInputStream dis) {
+        try {
             DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
             long recibidos = 0;
             // paquete para el contenido del archivo
@@ -34,21 +55,49 @@ public class Cliente {
                 recibidos = recibidos + n;
                 porcentaje = (int) (recibidos * 100 / tam);
                 System.out.print("Recibido: " + porcentaje + "%\r");
-            } // While
+            }
             System.out.print("\nArchivo recibido.\n");
+            dos.close();
 
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    // Deserializando archivo
+    public static void deserialize(String nombre, int numProducts) {
+        try {
             // Deserializando el archivo
             FileInputStream file = new FileInputStream(nombre);
             ObjectInputStream ois = new ObjectInputStream(file);
             System.out.println("Deserializando txt...");
-            System.out.println("Puedes consultar el catalogo en el archivo txt, al igual que las imagenes que se te mandaron");
-
-            dos.close();
-            dis.close();
+            System.out.println("El catalogo es: ");
+            for (int i = 0; i < numProducts; i++) {
+                Producto aux = (Producto) ois.readObject();
+                System.out.println("Num Producto: " + i++);
+                System.out.println("Nombre: " + aux.name);
+                System.out.println("Precio: $" + aux.price);
+                System.out.println("Descripcion: " + aux.desc);
+                System.out.println("Stock: " + aux.stock);
+                System.out.println();
+            }
             ois.close();
-            cl.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            // TODO: handle exception
+        }
+    }
+
+    // Comprando un producto
+    public static void buying(BufferedReader br, DataOutputStream dos) {
+        try {
+            System.out.println("¿Que producto desea comprar?");
+            System.out.println("Ingresa el numero del producto");
+            int num = Integer.parseInt(br.readLine());
+            System.out.println("Se estan comprobando existencias de su producto, por favor espere");
+            dos.writeInt(num);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
         }
     }
 }
--- Primer-parcial/Practica/Practica/Servidor/Servidor.java
+++ Primer-parcial/Practica/Practica/Servidor/Servidor.java
@@ -1,68 +1,112 @@
-package Servidor;
 
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class Servidor {
+    static ArrayList<Producto> catalogo = new ArrayList<Producto>();
+
     public static void main(String[] args) {
         try {
             ServerSocket s = new ServerSocket(7000);
             for (;;) {
                 Socket cl = s.accept();
                 System.out.println("Conexión establecida desde" + cl.getInetAddress() + ":" + cl.getPort());
+                DataInputStream dis = new DataInputStream(cl.getInputStream());
+                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
 
                 byte[] img = new byte[100];
 
                 // Agregando productos al catalogo
-                Catalogo ct = new Catalogo();
-                ct.addProduct("Playera azul", 200, "Playera de tela 100% algodon", 10, img);
-                ct.addProduct("Playera roja", 200, "Playera de tela 100% algodon", 10, img);
-                ct.addProduct("Playera verde", 200, "Playera de tela 100% algodon", 10, img);
-                ct.addProduct("Playera amarilla", 200, "Playera de tela 100% algodon", 10, img);
-                ct.addProduct("Playera magenta", 200, "Playera de tela 100% algodon", 10, img);
-                ct.addProduct("Playera rosa", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera azul", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera roja", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera verde", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera amarilla", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera magenta", 200, "Playera de tela 100% algodon", 10, img);
+                addProduct("Playera rosa", 200, "Playera de tela 100% algodon", 10, img);
 
-                // Serializacion del catalogo
-                // crear el archivo
-                File file = new File("Catalogo.txt");
-                FileOutputStream fileS = new FileOutputStream(file);
-                ObjectOutputStream oos = new ObjectOutputStream(fileS);
+                // Serializar y enviar el catalogo
+                sendCatalogue(dos);
 
-                // serializar el objeto
-                System.out.println("\nSerializando...");
-                oos.writeObject(ct);
-                oos.close();
-
-                // Enviando archivo al servidor
-                DataInputStream dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
-                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
-                // nombre del archivo
-                dos.writeUTF(file.getName());
-                dos.flush();
-                // tamaño
-                long tam = file.length();
-                dos.writeLong(tam);
-                dos.flush();
-                byte[] b = new byte[6 * 1024];
-                long enviados = 0;
-                int porcentaje, n;
-                while (enviados < tam) {
-                    n = dis.read(b);
-                    // se escribe el contenido del archivo
-                    dos.write(b, 0, n);
-                    dos.flush();
-                    enviados = enviados + n;
-                    porcentaje = (int) (enviados * 100 / tam);
-                    System.out.print("Enviado: " + porcentaje + "%\r");
+                // Recibir el producto que el usuario quiere comprar
+                int numProduct = dis.readInt();
+                // Comprobacion de exitencias
+                int exist = catalogo.get(numProduct).getStock();
+                if(exist >= 0){
+                    System.out.println("Producto en existencia");
+                    System.out.println("Reduciendo el stock de exitencias");
+                    catalogo.get(numProduct).setStock(exist - 1);
+                    sendCatalogue(dos);
                 }
-                System.out.print("\nArchivo enviado\n");
-                dis.close();
-                dos.close();
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         } // catch
+    }
+
+    // Funcion para agregar producto
+    public static void addProduct(String name, int price, String desc, int stock, byte[] img) {
+        catalogo.add(new Producto(name, price, desc, stock, img));
+    }
+
+    // Funcion para reducir el stock
+    public static void reduceStock(int pos) {
+        int stock = catalogo.get(pos).getStock();
+        catalogo.get(pos).setStock(stock - 1);
+    }
+
+    // funcion para borrar productos
+    public static void deleteProduct(int pos) {
+        catalogo.remove(pos);
+    }
+
+    // Serializar y enviar catalogo
+    public static void sendCatalogue(DataOutputStream dos) {
+        try {
+            // Serializacion del catalogo
+            // crear el archivo
+            File file = new File("Catalogo.txt");
+            FileOutputStream fileS = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fileS);
+
+            // serializar los productos
+            System.out.println("\nSerializando...");
+            for (int i = 0; i < catalogo.size(); i++) {
+                oos.writeObject(catalogo.get(i));
+            }
+            oos.close();
+
+            // Enviando archivo al servidor
+            DataInputStream dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
+
+            // numero de productos que va a recibir el cliente
+            dos.writeInt(catalogo.size());
+            // nombre del archivo
+            dos.writeUTF(file.getName());
+            dos.flush();
+            // tamaño
+            long tam = file.length();
+            dos.writeLong(tam);
+            dos.flush();
+            byte[] b = new byte[6 * 1024];
+            long enviados = 0;
+            int porcentaje, n;
+            while (enviados < tam) {
+                n = dis.read(b);
+                // se escribe el contenido del archivo
+                dos.write(b, 0, n);
+                dos.flush();
+                enviados = enviados + n;
+                porcentaje = (int) (enviados * 100 / tam);
+                System.out.print("Enviado: " + porcentaje + "%\r");
+            }
+            System.out.print("\nArchivo enviado\n");
+            dis.close();
+            dos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
